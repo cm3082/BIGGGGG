03c5fe79913c996718af7d0466c45fc53f5698ec
caiming <caiming@genomics.cn>
2018-06-26 11:30:29 +0800

    springboot整合定时任务及
    异步任务：应用场景：发送短信、发送邮件、app消息推送、节省运维凌晨发布任务时间

--- Common-technologies-springboot/src/main/java/com/cm/CommonTechnologiesSpringbootApplication.java
+++ Common-technologies-springboot/src/main/java/com/cm/CommonTechnologiesSpringbootApplication.java
@@ -3,6 +3,8 @@ package com.cm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -12,9 +14,9 @@ import tk.mybatis.spring.annotation.MapperScan;
 //扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
 @ComponentScan(basePackages= {"com.cm", "org.n3r.idworker"})
 //开启定时任务
-//@EnableScheduling
+@EnableScheduling
 //开启异步调用方法
-//@EnableAsync
+@EnableAsync
 public class CommonTechnologiesSpringbootApplication {
 
 	public static void main(String[] args) {
